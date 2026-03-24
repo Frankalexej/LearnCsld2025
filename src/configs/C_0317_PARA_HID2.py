8f5f3a0550f5d65078b25a60e0827b9a43488ef8
Frankalexej <tt115889@connect.hku.hk>
2026-03-24 09:17:52 +0800

    20260324 + 1

--- src/configs/C_0317_PARA_HID2.py
+++ src/configs/C_0317_PARA_HID2.py
@@ -6,7 +6,7 @@ READ_BASE_PATH = '/mnt/storage/ldl_linguistics/PhonGen2025'
 MODEL_LOAD_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025'
 WRITE_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025'
 CSV_PATH = '/mnt/storage/ldl_linguistics/PhonGen2025/data_251011/data_train_phase1/metadata_train_phase1.csv'
-CSV_PATH2 = '/mnt/storage/ldl_linguistics/PhonGen2025/data_251011/data_train_phase2_a/metadata_train_phase2_a.csv'
+CSV_PATH2 = '/mnt/storage/franklhtan/projects/LearnCsld2025/data/data_train_phase2_a/data_train_phase2_a.csv'
 CSV_PATH3 = '/mnt/storage/ldl_linguistics/PhonGen2025/data_251011/data_test_a/metadata_test_a.csv'
 CSV_PATH4 = '/mnt/storage/franklhtan/projects/LearnCsld2025/data/data_test_1/metadata_test_1.csv'
 DEVICE = 'cuda'
@@ -22,6 +22,6 @@ BASE_SEED = 20260315          # shared across the entire project
 DETERMINISTIC = False         # True only if you need strict determinism
 
 SIMILARITY = "euclidean"
-RUN_NAMES = ['0317_RCRC_PARA_EWC_1e3_HID2', '0317_RCRC_PARA_NFZ_1e3_HID2']  # '0214_RCRC_PARA_NFZ_1e4', '0214_RCRC_PARA_NFZ_1e5', '0214_RCRC_PARA_EWC_1e4'
+RUN_NAMES = ['0317_RCRC_PARA_EWC_1e3_HID2', '0317_RCRC_PARA_NFZ_1e3_HID2', '0317_RCCL_PARA_EWC_1e3_HID2', '0317_RCCL_PARA_NFZ_1e3_HID2']  
 RUN_TIMES_START = 1
 RUN_TIMES_END = RUN_TIMES_START+10
