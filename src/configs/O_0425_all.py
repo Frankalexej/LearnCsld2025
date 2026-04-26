b00fb8f2d9a02949d36ffee176e157f17f327f3b
Frankalexej <tt115889@connect.hku.hk>
2026-04-26 11:24:41 +0800

    20260426 + 3

---
+++ src/configs/O_0425_all.py
@@ -0,0 +1,10 @@
+# Configuration parameters
+READ_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025/eval_outputs'
+WRITE_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025/observe_outputs'
+EPOCH_START = 10
+EPOCH_END = 601 # changed to 201 for NPT (no-pretrain) runs, which have 200 epochs total
+EPOCH_INTERVAL = 10
+RUN_TIMES_START = 1
+RUN_TIMES_END = 11
+RUN_NAMES = ['0425_RCRC_AMP_EWCp1_1e4_HID2']
+RUN_NAMES_SHORT = RUN_NAMES
