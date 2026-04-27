6481d092cddf7a11f915fa0fd2f55d2ea3d19cd5
Frankalexej <tt115889@connect.hku.hk>
2026-04-27 23:25:21 +0800

    20260427 + 6

---
+++ src/configs/O_0427_UQL_HID2.py
@@ -0,0 +1,10 @@
+# Configuration parameters
+READ_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025/eval_outputs'
+WRITE_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025/observe_outputs'
+EPOCH_START = 10
+EPOCH_END = 301 # changed to 201 for NPT (no-pretrain) runs, which have 200 epochs total
+EPOCH_INTERVAL = 10
+RUN_TIMES_START = 1
+RUN_TIMES_END = 6
+RUN_NAMES = ['0427_RCRC_UQL_EWCp1_1e4_HID2', '0427_RCCL_UQL_EWCp1_1e4_HID2']
+RUN_NAMES_SHORT = RUN_NAMES
