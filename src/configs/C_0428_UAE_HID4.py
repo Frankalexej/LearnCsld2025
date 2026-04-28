975a5bed346ce6949bf6c772f035259971f047c9
Frankalexej <tt115889@connect.hku.hk>
2026-04-28 13:54:48 +0800

    20260428 + 2

---
+++ src/configs/C_0428_UAE_HID4.py
@@ -0,0 +1,30 @@
+# Configuration parameters
+BATCH_SIZE = 32
+PRE_EPOCHS = 100
+POST_EPOCHS = 200
+READ_BASE_PATH = '/mnt/storage/ldl_linguistics/PhonGen2025'
+MODEL_LOAD_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025'
+WRITE_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025'
+DATASET_NAME = "data_VW"
+CONDITION_SUFFIX = "equal"
+CSV_PATH = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_train_{CONDITION_SUFFIX}/metadata_train_{CONDITION_SUFFIX}.csv'
+CSV_PATH2 = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_train_{CONDITION_SUFFIX}/metadata_train_{CONDITION_SUFFIX}.csv'
+CSV_PATH3 = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_test_{CONDITION_SUFFIX}/metadata_test_{CONDITION_SUFFIX}.csv'
+CSV_PATH4 = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_test_{CONDITION_SUFFIX}/metadata_test_{CONDITION_SUFFIX}.csv'
+DEVICE = 'cuda'
+IN_FEATURES = 15
+HID_FEATURES = 4
+OUT_FEATURES = 4
+
+L1_MANIPULANT_SELECT = ['e', 'o', 'i', 'u', 'ih', 'uh', 'iS', 'uS', 'ihL', 'uhL']  # L1: s vs c
+L2_MANIPULANT_SELECT = ['e', 'o', 'i', 'u', 'ih', 'uh', 'iS', 'uS', 'ihL', 'uhL']  # L2: VERT still tsh vs sh. 
+
+# Seed control
+BASE_SEED = 20260428          # shared across the entire project
+DETERMINISTIC = False         # True only if you need strict determinism
+
+SIMILARITY = "euclidean"
+RUN_NAMES = ['0428_RCRC_UQL_EWCp2_1e4_HID4', '0428_RCCL_UQL_EWCp2_1e4_HID4']
+WRITE_RUN_NAMES = ['0428_RCRC_UAE_EWCp2_1e4_HID4', '0428_RCCL_UAE_EWCp2_1e4_HID4']
+RUN_TIMES_START = 1
+RUN_TIMES_END = RUN_TIMES_START+5
