b54ceffc4b77face986f6457b5a325e7aff1eb09
Frankalexej <tt115889@connect.hku.hk>
2026-06-05 01:13:13 +0800

    20260605 + 2

---
+++ src/configs/T_0604_RCRC_VERT_EWCp1_1e4_HID2.py
@@ -0,0 +1,39 @@
+# 0429: Equal distribution. 
+
+# Configuration parameters
+BATCH_SIZE = 32
+PRE_EPOCHS = 200
+POST_EPOCHS = 400
+LR = 1e-4
+L2_LR = 1e-4
+
+DATASET_NAME = "data_CSLD"
+CONDITION_SUFFIX = "equal"
+CSV_PATH = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_train_{CONDITION_SUFFIX}/metadata_train_{CONDITION_SUFFIX}.csv'
+CSV_PATH2 = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_train_{CONDITION_SUFFIX}/metadata_train_{CONDITION_SUFFIX}.csv'
+CSV_PATH3 = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_test_{CONDITION_SUFFIX}/metadata_test_{CONDITION_SUFFIX}.csv'
+CSV_PATH4 = f'/mnt/storage/franklhtan/projects/LearnCsld2025/{DATASET_NAME}/data_test_{CONDITION_SUFFIX}/metadata_test_{CONDITION_SUFFIX}.csv'  # NEW 20260203, I just suddenly found that we didn't have testing on L1. 
+
+DEVICE = 'cuda'
+IN_FEATURES = 15
+HID_FEATURES = 2
+OUT_FEATURES = 2
+OUT_FEATURES_2 = 2
+# Notice that because we are desigining this to be only either two identical CLs or RCCL or RCRC, so number of features do not differ. 
+PRE_METHOD = "RC"
+POST_METHOD = "RC"
+L1_MANIPULANT_SELECT = ['s', 'c']  # L1: i u e o
+L2_MANIPULANT_SELECT = ['tsh', 'sh']  # L2: i u ih uh
+FREEZE_FOR_L2 = False            # whether to freeze the L1 encoder when training on L2
+CONSOLIDATION_METHOD = "EWC"
+CONSOLIDATION_STRENGTH = 10.0   # trying stronger consolidation strength. p9 = 1e9. 
+
+# Seed control
+BASE_SEED = 20260604          # shared across the entire project
+DETERMINISTIC = False         # True only if you need strict determinism
+
+SIMILARITY = "euclidean"
+RUN_NAME = '0604_RCRC_VERT_EWCp3_1e4_HID2'
+RUN_TIMES_START = 1
+RUN_TIMES_END = RUN_TIMES_START+10
+SAMPLE_LIST = []
