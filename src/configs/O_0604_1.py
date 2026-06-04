95bbee8d7c6436109fd0f14a13be748ce2954315
Frankalexej <tt115889@connect.hku.hk>
2026-06-04 16:31:54 +0800

    20260604 + 1

---
+++ src/configs/O_0604_1.py
@@ -0,0 +1,10 @@
+# Configuration parameters
+READ_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025/eval_outputs'
+WRITE_BASE_PATH = '/mnt/storage/franklhtan/projects/LearnCsld2025/observe_outputs'
+EPOCH_START = 10
+EPOCH_END = 601 # changed to 201 for NPT (no-pretrain) runs, which have 200 epochs total
+EPOCH_INTERVAL = 10
+RUN_TIMES_START = 6
+RUN_TIMES_END = 11
+RUN_NAMES = ['0604_RCRC_PARA_EWCp3_1e4_HID2', '0604_RCRC_PARA_EWCn3_1e4_HID2', '0604_RCRC_VERT_EWCn3_1e4_HID2']  # '0603_RCRC_PARA_EWCp3_1e4_HID2', 
+RUN_NAMES_SHORT = RUN_NAMES
